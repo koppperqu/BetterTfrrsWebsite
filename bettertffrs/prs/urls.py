07de33b0bf680b76ec8ee58aed078937b3234f85
koppperqu <kopp.austin@outlook.com>
2023-01-16 21:39:08 -0600

    Need to setup webscraper to put data into the DB

--- bettertffrs/prs/urls.py
+++ bettertffrs/prs/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recent', views.recentprs, name='recentprs'),
+    path('athlete/<int:athlete_id>', views.athlete, name='athlete'),
+    path('event/<int:event_id>', views.event, name='event'),
 ]
