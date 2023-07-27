952d7020563f4ff85a1d918e2275913233ddcab3
kopperqu <email@email.com>
2023-07-27 10:34:54 -0500

    Turn off debug

--- bettertffrs/bettertffrs/settings.py
+++ bettertffrs/bettertffrs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qc8@tp92^i()$szt52bf45jow-&unsqfbvw(od*uozsch^dck#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
